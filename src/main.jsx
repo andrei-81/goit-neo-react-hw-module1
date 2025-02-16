64221937c0938cd7cb2eb84927bca486fa8080de
Andrii Hordiichuk <andrei.g81@gmail.com>
2025-02-16 19:16:03 +0200

    style fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { StrictMode } from 'react'
-import './style.css'
 import App from './components/App.jsx'
 
 createRoot(document.getElementById('app')).render(
